8ac70cea7c154dbbf101bd3a23790d503901c3ee
AlexSutila <alexsutila2020@gmail.com>
2023-04-28 14:50:57 -0700

    Add some stuff

---
+++ get_dataset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir data
+
+# https://archive.ics.uci.edu/ml/machine-learning-databases/census1990-mld/
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/census1990-mld/USCensus1990.attributes.txt -O data/attributes.txt
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/census1990-mld/USCensus1990.data.txt -O data/USCensus1990.csv
+
